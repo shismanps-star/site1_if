18ac64b378faedd4e2bffe3f275101c87cdd4155
shismanps-star <shisman.ps@gmail.com>
2025-12-07 20:20:36 +0200

    js2

--- docs/js/script1.js
+++ docs/js/script1.js
@@ -1,4 +1,7 @@
 var btn1 = document.getElementById("btn-1");
+var input_name = document.getElementById("input-name");
 btn1.addEventListener("click", () => {
-    alert("Hello World!");
+    var name = input_name.value;
+    console.log(name);
+alert(`Hello ${name}!`);
 });
